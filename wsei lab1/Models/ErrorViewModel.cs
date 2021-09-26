985f2d5cab33d07a1c257a66a1a95465b8344e02
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2021-09-26 10:57:05 +0200

    Dodaj pliki projektów.

---
+++ wsei lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wsei_lab1.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
