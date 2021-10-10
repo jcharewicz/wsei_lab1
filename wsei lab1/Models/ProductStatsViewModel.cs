f93ece2b7b40c8429896338358491c3b62419078
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2021-10-10 11:08:10 +0200

    add product controller

---
+++ wsei lab1/Models/ProductStatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wsei_lab1.Models
+{
+	public class ProductStatsViewModel
+	{
+		public int NameLength { get; set; }
+		public int DescriptionLength { get; set; }
+	}
+}
